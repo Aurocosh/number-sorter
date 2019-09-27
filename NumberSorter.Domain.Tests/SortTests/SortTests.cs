7a899fb9bb1751a5091086b6c5e767ff1c39b959
Aurocosh <Aurocosh@gmail.com>
2019-09-28 00:06:47 +0700

    Added more tests

--- NumberSorter.Domain.Tests/SortTests/SortTests.cs
+++ NumberSorter.Domain.Tests/SortTests/SortTests.cs
@@ -1,8 +1,9 @@
-﻿using NumberSorter.Domain.Algorhythm;
-using NumberSorter.Domain.Logic.Algorhythm;
+﻿using AutoFixture.Xunit2;
+using NumberSorter.Domain.Algorhythm;
 using NumberSorter.Domain.Logic.Comparer;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using Xunit;
 
 namespace NumberSorter.Domain.Tests.SortTests
@@ -25,14 +26,23 @@ namespace NumberSorter.Domain.Tests.SortTests
         [ClassData(typeof(StaticRandom100IntegerListGenerator))]
         public void TestSort100RandomStatic(List<int> testData)
         {
-            _sort.Sort(testData);
-            bool fullySorted = ListUtility.IsSorted(testData, _comparer);
-            Assert.True(fullySorted);
+            TestSort(testData);
         }
 
         [Theory]
         [ClassData(typeof(DynamicRandom100IntegerListGenerator))]
         public void TestSort100RandomDynamic(List<int> testData)
+        {
+            TestSort(testData);
+        }
+
+        [Theory, AutoData]
+        public void TestAutoData([MinLength(10), MaxLength(50)]int[] testData)
+        {
+            TestSort(testData);
+        }
+
+        private void TestSort(IList<int> testData)
         {
             _sort.Sort(testData);
             bool fullySorted = ListUtility.IsSorted(testData, _comparer);
