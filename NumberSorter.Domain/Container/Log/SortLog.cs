0dc7ac425e65939f6e72bb646f14b7c8f0b0c556
Aurocosh <Aurocosh@gmail.com>
2019-10-19 00:25:14 +0700

    Added missing files

---
+++ NumberSorter.Domain/Container/Log/SortLog.cs
@@ -0,0 +1,41 @@
+﻿using NumberSorter.Core.Logic.Utility;
+using NumberSorter.Domain.Container.Actions;
+using NumberSorter.Domain.Container.Actions.Base;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberSorter.Domain.Container
+{
+    public class SortLog<T> where T : IEquatable<T>
+    {
+        public LogSummary Summary { get; }
+        public SortState<T> InputState { get; }
+        public SortState<T> FinalState { get; }
+        public IReadOnlyList<LogAction<T>> ActionLog { get; }
+
+        public SortLog()
+        {
+            Summary = new LogSummary();
+            InputState = new SortState<T>(Array.Empty<T>());
+            FinalState = new SortState<T>(Array.Empty<T>());
+            ActionLog = new List<LogAction<T>>();
+        }
+
+        public SortLog(string inputName, Guid inputId, IReadOnlyList<T> inputState, IReadOnlyList<T> finalState, IReadOnlyList<LogAction<T>> actionLog, IComparer<T> comparer, float elapsedTime, string algorhythmName)
+        {
+            var fullySorted = ListUtility.IsSorted(finalState, comparer);
+            var totalReadCount = actionLog.Sum(x => x.ReadCount);
+            var totalWriteCount = actionLog.Sum(x => x.WriteCount);
+            var totalComparassionCount = actionLog.Sum(x => x.ComparassionCount);
+            Summary = new LogSummary(fullySorted, elapsedTime, algorhythmName, inputId, inputName, inputState.Count, totalReadCount, totalWriteCount, totalComparassionCount);
+
+            InputState = new SortState<T>(inputState.ToArray());
+            FinalState = new SortState<T>(finalState.ToArray());
+            ActionLog = actionLog;
+        }
+    }
+}
---
+++ NumberSorter.Domain/Container/Log/SortSummary.cs
@@ -0,0 +1,74 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace NumberSorter.Domain.Container
+{
+    public sealed class LogSummary
+    {
+        public DateTime Created { get; }
+        public bool FullySorted { get; }
+        public float ElapsedTime { get; }
+        public string AlgorhythmName { get; }
+
+        public Guid InputId { get; }
+        public Guid FinishId { get; }
+        public string InputName { get; }
+
+        public int ElementCount { get; }
+        public int TotalReadCount { get; }
+        public int TotalWriteCount { get; }
+        public int TotalComparassionCount { get; }
+
+        public LogSummary()
+        {
+            Created = DateTime.Now;
+            FullySorted = true;
+            ElapsedTime = 0;
+            AlgorhythmName = "";
+
+            InputName = "Empty";
+            InputId = Guid.Empty;
+            FinishId = Guid.Empty;
+
+            ElementCount = 0;
+            TotalReadCount = 0;
+            TotalWriteCount = 0;
+            TotalComparassionCount = 0;
+        }
+
+        public LogSummary(bool fullySorted, float elapsedTime, string algorhythmName, Guid inputId, string inputName, int elementCount, int totalReadCount, int totalWriteCount, int totalComparassionCount)
+        {
+            Created = DateTime.Now;
+            FullySorted = fullySorted;
+            ElapsedTime = elapsedTime;
+            AlgorhythmName = algorhythmName;
+
+            InputId = inputId;
+            InputName = inputName;
+            FinishId = Guid.NewGuid();
+
+            ElementCount = elementCount;
+            TotalReadCount = totalReadCount;
+            TotalWriteCount = totalWriteCount;
+            TotalComparassionCount = totalComparassionCount;
+        }
+
+        [JsonConstructor]
+        public LogSummary(DateTime created, bool fullySorted, float elapsedTime, string algorhythmName, Guid inputId, Guid finishId, string inputName, int elementCount, int totalReadCount, int totalWriteCount, int totalComparassionCount)
+        {
+            Created = created;
+            FullySorted = fullySorted;
+            ElapsedTime = elapsedTime;
+            AlgorhythmName = algorhythmName;
+
+            InputId = inputId;
+            FinishId = finishId;
+            InputName = inputName;
+
+            ElementCount = elementCount;
+            TotalReadCount = totalReadCount;
+            TotalWriteCount = totalWriteCount;
+            TotalComparassionCount = totalComparassionCount;
+        }
+    }
+}
