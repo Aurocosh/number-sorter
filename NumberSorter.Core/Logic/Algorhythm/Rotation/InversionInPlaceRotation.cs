e5b58062b5bb9faea7aa5d5f59fe6c5009739653
Aurocosh <Aurocosh@gmail.com>
2019-10-19 15:03:56 +0700

    Added in inversion based sort run rotation

---
+++ NumberSorter.Core/Logic/Algorhythm/Rotation/InversionInPlaceRotation.cs
@@ -0,0 +1,27 @@
+﻿using NumberSorter.Core.Logic.Algorhythm.Merge.Base;
+using NumberSorter.Core.Logic.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NumberSorter.Core.Logic.Algorhythm
+{
+    public class InversionInPlaceRotation<T> : ILocalRotationAlgothythm<T>
+    {
+        public InversionInPlaceRotation()
+        {
+        }
+
+        public void Rotate(IList<T> list, SortRun leftRun, SortRun rightRun)
+        {
+            var fullRun = new SortRun(leftRun.Start, leftRun.Length + rightRun.Length);
+
+            var newLeftRun = new SortRun(leftRun.Start, rightRun.Length);
+            var newRightRun = new SortRun(leftRun.Start + rightRun.Length, leftRun.Length);
+
+            SortRunUtility.InvertRun(list, fullRun);
+            SortRunUtility.InvertRun(list, newLeftRun);
+            SortRunUtility.InvertRun(list, newRightRun);
+        }
+    }
+}
---
+++ NumberSorter.Domain.Tests/RotationTests/InversionInPlaceRotationTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NumberSorter.Core.Algorhythm;
+using NumberSorter.Core.Logic.Algorhythm;
+using NumberSorter.Core.Logic.Algorhythm.Merge.Base;
+using NumberSorter.Domain.Tests.RotationTests.Base;
+
+namespace NumberSorter.Domain.Tests.RotationTests
+{
+    public class InversionInPlaceRotationTests : RotationTestsBase
+    {
+        protected override ILocalRotationAlgothythm<int> GetAlgorhythm()
+        {
+            return new InversionInPlaceRotation<int>();
+        }
+    }
+}
