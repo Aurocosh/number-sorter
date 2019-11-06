a6ad6add56197e9bbceb5f4b618b740b1978d62f
Aurocosh <Aurocosh@gmail.com>
2019-11-06 19:13:46 +0700

    Benchmark tweak

--- NumberSorter.Domain.Benchmark/Benchmarks/KWayMerge/KWayMergeSortGroupedBenchmarks.cs
+++ NumberSorter.Domain.Benchmark/Benchmarks/KWayMerge/KWayMergeSortGroupedBenchmarks.cs
@@ -12,7 +12,7 @@ namespace NumberSorter.Domain.Benchmark.Benchmarks
     {
         protected override ISortAlgorhythm<int> GetAlgorhythm(IComparer<int> comparer)
         {
-            return new KWayMergeSort<int>(comparer, new BinarySortFactory(), new GroupingRunLocatorFactory(32, new BinarySortFactory()), new BiasedBinaryPositionLocatorFactory(2), 32);
+            return new KWayMergeSort<int>(comparer, new BinarySortFactory(), new GroupingRunLocatorFactory(32, new BinarySortFactory()), new BiasedBinaryPositionLocatorFactory(2), 256);
         }
     }
 }
