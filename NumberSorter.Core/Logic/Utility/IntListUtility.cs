6fd434459240994dca07e9f6269fb6f2d3bea263
Aurocosh <Aurocosh@gmail.com>
2019-11-08 16:17:29 +0700

    Refactoring

--- NumberSorter.Core/Logic/Utility/IntListUtility.cs
+++ NumberSorter.Core/Logic/Utility/IntListUtility.cs
@@ -18,7 +18,9 @@ namespace NumberSorter.Core.Logic.Utility
             int leftIndex = startingIndex;
             int rightIndex = startingIndex + length - 1;
             int swapCount = length / 2;
-            for (int i = 0; i < swapCount; i++)
+
+            int indexLimit = startingIndex + swapCount;
+            while (leftIndex != indexLimit)
             {
                 int temp = -list[leftIndex];
                 list[leftIndex] = -list[rightIndex];
