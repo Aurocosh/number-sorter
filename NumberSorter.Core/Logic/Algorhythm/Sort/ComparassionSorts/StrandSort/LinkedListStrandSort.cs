ccf7974aac83b801733b6033cd9f88d50932595c
Aurocosh <Aurocosh@gmail.com>
2020-07-23 23:43:52 +0700

    Added linked list strand sort

---
+++ NumberSorter.Core/Logic/Algorhythm/Sort/ComparassionSorts/StrandSort/LinkedListStrandSort.cs
@@ -0,0 +1,60 @@
+﻿using NumberSorter.Core.Logic.Algorhythm.LocalMerge.Base;
+using NumberSorter.Core.Logic.Factories.LocalMerge.Base;
+using System.Collections.Generic;
+
+namespace NumberSorter.Core.Logic.Algorhythm
+{
+    public class LinkedListStrandSort<T> : GenericSortAlgorhythm<T>
+    {
+        private ILocalMergeAlgothythm<T> LocalMergeAlgorhythm { get; }
+
+        public LinkedListStrandSort(IComparer<T> comparer, ILocalMergeFactory localMergeFactory) : base(comparer)
+        {
+            LocalMergeAlgorhythm = localMergeFactory.GetLocalMerge(comparer);
+        }
+
+        public override void Sort(IList<T> list, int startingIndex, int length)
+        {
+            var source = new LinkedList<T>();
+            var buffer = new LinkedList<T>();
+
+            int indexLimit = startingIndex + length;
+            for (int i = startingIndex; i < indexLimit; i++)
+                source.AddLast(list[i]);
+
+            int resultSize = 0;
+            int targetIndex = startingIndex;
+            while (source.Count > 0)
+            {
+                T nextInBuffer = source.First.Value;
+                buffer.AddLast(nextInBuffer);
+                source.RemoveFirst();
+
+                var currentNode = source.First;
+                while (currentNode != null)
+                {
+                    var nextNode = currentNode.Next;
+                    T nextInSource = currentNode.Value;
+                    if (Compare(nextInSource, nextInBuffer) >= 0)
+                    {
+                        nextInBuffer = nextInSource;
+                        buffer.AddLast(nextInSource);
+                        source.Remove(currentNode);
+                    }
+                    currentNode = nextNode;
+                }
+
+                var leftRun = new SortRun(startingIndex, resultSize);
+                var rightRun = new SortRun(targetIndex, buffer.Count);
+
+                foreach (var value in buffer)
+                    list[targetIndex++] = value;
+
+                LocalMergeAlgorhythm.Merge(list, leftRun, rightRun);
+
+                resultSize += buffer.Count;
+                buffer.Clear();
+            }
+        }
+    }
+}
---
+++ NumberSorter.Domain.Tests/SortTests/ComparassionSorts/Strand/LinkedListStrandSortTests.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using NumberSorter.Core.Algorhythm;
+using NumberSorter.Core.Logic.Algorhythm;
+using NumberSorter.Core.Logic.Factories.LocalMerge;
+using NumberSorter.Core.Logic.Factories.PositionLocator;
+using NumberSorter.Domain.Tests.SortTests.Base;
+
+namespace NumberSorter.Domain.Tests.SortTests
+{
+    public class LinkedListStrandSortTests : SortTestsBase
+    {
+        protected override ISortAlgorhythm<int> GetAlgorhythm(IComparer<int> comparer)
+        {
+            var merge = new IntervalMergeFactory(new BiasedBinaryPositionLocatorFactory(1));
+            return new LinkedListStrandSort<int>(comparer, merge);
+        }
+    }
+}
